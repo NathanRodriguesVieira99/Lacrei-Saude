b838181973447d0f9988cfd9f5bbaf2d5ab7d433
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-19 16:10:21 -0300

    feat: update global styles

--- src/styles/global/index.ts
+++ src/styles/global/index.ts
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
   list-style: none;
-  text-decoration: none;
+  scroll-behavior: smooth;
 }
+a{ text-decoration: none;}
 `;
