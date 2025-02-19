fa6dbb4e80ea4c8639783fe299e02f7ee4bdbab4
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-18 21:03:39 -0300

    feat: add more colors

--- src/styles/colors/index.ts
+++ src/styles/colors/index.ts
@@ -1,5 +1,15 @@
 export const colors = {
-  background:{
-    default400: "#FFFFFF"
-  }
+  background: {
+    default400: "#FFFFFF",
+    disabled400: "#F0F0F0",
+  },
+  border: {
+    border_accent400: "#018762",
+  },
+  hover: {
+    accent100: "#DFF2ED",
+  },
+  text: {
+    accent400: "#018762",
+  },
 };
