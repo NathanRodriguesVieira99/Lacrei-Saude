b4c99ef209e1f64b22bb814fa2c28d81075a27a1
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-20 21:49:04 -0300

    test: clean up rendering tests for Inicio component

--- src/app/Inicio/page.test.tsx
+++ src/app/Inicio/page.test.tsx
@@ -1,10 +1,8 @@
 import Inicio from "./page";
-import { screen, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 describe("Rendering tests to Initial Page", () => {
   it("should render initial Page", () => {
     render(<Inicio />);
-    // const PageTitle = screen.getByText(/Boas-vindas à Lacrei Saúde!/i);
-    // expect(PageTitle).toBeInTheDocument();
   });
 });
